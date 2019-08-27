e65d4d937612213e8d8cfc01600732d48e41ae7f
Matt Uyehara <laieboy@gmail.com>
2019-08-26 20:58:50 -0600

    Day 1 of rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+  resources :pages
+  # get
+  # post 
+  # put
+  # delete
+  # https
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
